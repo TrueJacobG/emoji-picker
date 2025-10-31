a176b0e0e96cafb5c5ad0b3d40bc60a40a5f4ad2
TrueJacobG <jakubgradzewicz1309@gmail.com>
2025-10-31 16:50:11 +0100

    Initial Commit

---
+++ emoji-pickerTests/emoji_pickerTests.swift
@@ -0,0 +1,17 @@
+//
+//  emoji_pickerTests.swift
+//  emoji-pickerTests
+//
+//  Created by Jakub Gradzewicz on 31/10/2025.
+//
+
+import Testing
+@testable import emoji_picker
+
+struct emoji_pickerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
